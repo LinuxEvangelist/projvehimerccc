8bcd395430d8cdf9354f57f847503d3c66d36e58
vodosioksvaas <riczor16@gmail.com>
2015-10-07 16:52:34 -0500

    07octubre(1)

---
+++ src/main/java/com/reglamb/projvehimerc/controller/MainController.java
@@ -0,0 +1,20 @@
+package com.reglamb.projvehimerc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MainController {
+	@Autowired
+	protected UserDetailsService userService;
+	@RequestMapping(value = { "/views**" },method = RequestMethod.GET)
+	public String getJsp(ModelMap model) {
+
+		model.addAttribute("message", "Spring 3 MVC Hello World");
+		return "views/flowjsp/index";
+	}
+}
